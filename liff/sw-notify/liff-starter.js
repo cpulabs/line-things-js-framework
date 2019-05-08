f7280111b64950daca159dbe5f3369124047b62c
ito.takahiro <ito.takahiro@linecorp.com>
2019-05-08 16:36:02 +0900

    update

--- liff/sw-notify/liff-starter.js
+++ liff/sw-notify/liff-starter.js
@@ -136,7 +136,7 @@ function connectDevice(device) {
             );
 
             setup(things);
-            loop(things);
+            //loop(things);
         }).catch(e => {
             flashSDKError(e);
             onScreenLog(`ERROR on gatt.connect(${device.id}): ${e}`);
